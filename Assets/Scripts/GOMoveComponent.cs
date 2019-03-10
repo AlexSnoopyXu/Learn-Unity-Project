c7dc8b58fcaf757ec86d16e51b40d24869e0c8a5
AlexSnoopyXu <alexsnoopyxu@163.com>
2019-03-10 20:44:19 +0800

    简单守卫行为树

---
+++ Assets/Scripts/GOMoveComponent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GOMoveComponent : MonoBehaviour {
+
+    [Header("控制的GameObject")]
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        
+        if(h != 0)
+        {
+            target.position = new Vector3(target.position.x + 0.1f * (h > 0 ? 1 : -1), target.position.y, target.position.z);
+        }
+
+        if (v != 0)
+        {
+            target.position = new Vector3(target.position.x, target.position.y, target.position.z + 0.1f * (v > 0 ? 1 : -1));
+        }
+    }
+}
